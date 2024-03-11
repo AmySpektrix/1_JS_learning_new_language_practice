8c9be0669d5951dbc858d413dae130c5f6483ebd
Amy Brown <amy.brown@spektrix.com>
2024-03-11 16:41:49 +0000

    First Commit

---
+++ 1_Phase_One_The_Basics/getNumberSign.js
@@ -0,0 +1,5 @@
+const getNumberSign = (number) => {
+    if (number > 0) {return "Positive"}
+    else if (number === 0) {return "Zero"}
+    else {number < 0} {return "Negative"}
+}
---
+++ 1_Phase_One_The_Basics/test.js
@@ -0,0 +1,26 @@
+console.log("Hi Amy");
+
+// exercise 2
+
+function two() {
+  console.log(2 + 2);
+}
+
+let x;
+
+function xTimesThree() {
+  console.log(x * 3);
+}
+
+function yTimesTen(y) {
+  console.log(y * 10);
+}
+
+const myAge = 34;
+
+function myAgeYearOld() {console.log("I am " + myAge + " year's old")};
+
+let yourAge;
+
+function yourAgeYearOld() {console.log("I am " + yourAge + " year's old")};
+
